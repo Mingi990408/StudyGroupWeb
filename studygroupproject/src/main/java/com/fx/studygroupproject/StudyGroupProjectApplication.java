8b1ceff2f806c29e1dd8e1d40ae0c5ff75b086f0
개발하는 고라니 <alsrl990408@gmail.com>
2023-05-29 13:52:55 +0900

    SGW-2 Create Project
    프로젝트 생성하기

---
+++ studygroupproject/src/main/java/com/fx/studygroupproject/StudyGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fx.studygroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGroupProjectApplication.class, args);
+    }
+
+}
---
+++ studygroupproject/src/test/java/com/fx/studygroupproject/StudygroupprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fx.studygroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudygroupprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
