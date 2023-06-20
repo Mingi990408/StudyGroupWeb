ecf975fa70f054337293c9ce0bf4a8f7d9b41ecf
개발하는 고라니 <alsrl990408@gmail.com>
2023-06-20 21:29:32 +0900

    SGW-9 Email Auth
    이메일 인증

--- studygroupproject/src/main/java/com/fx/studygroupproject/user/contoller/UserController.java
+++ studygroupproject/src/main/java/com/fx/studygroupproject/user/contoller/UserController.java
@@ -1,6 +1,7 @@
 package com.fx.studygroupproject.user.contoller;
 
 import com.fx.studygroupproject.user.Member;
+import com.fx.studygroupproject.user.service.EmailService;
 import com.fx.studygroupproject.user.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -20,6 +23,7 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserController {
     private final MemberService memberService;
+    private final EmailService emailService;
 
     @PostMapping("/login-callback")
     public boolean loginCallback(String Email, String Password) {
@@ -27,13 +31,29 @@ public class UserController {
     }
 
     @PostMapping("signup-callback")
-    public String signupCallback(@ModelAttribute("member") @Valid Member member, BindingResult bindingResult) {
+    public String signupCallback(@ModelAttribute("member") @Valid Member member, HttpSession session, BindingResult bindingResult, Model model) throws Exception {
         if (bindingResult.hasErrors()) {
             return "signup";
         }
-        memberService.signup(member);
-        return "homepage";
+        session.setAttribute("member", member);
+        model.addAttribute("member", member);
+        emailService.sendAuthMessage(member);
+        return "checkEmail";
     }
+
+    @GetMapping("auth-check")
+    public void authCheck(@RequestParam String authCode, HttpSession session, HttpServletResponse response) {
+        String authCode1 = emailService.getAuthCode();
+
+        Member member = (Member) session.getAttribute("member");
+        if (authCode1.equals(authCode)) {
+            memberService.signup(member);
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
     @GetMapping("/email/duplicate-check")
     public void  emailDuplicateCheck(@RequestParam String email, HttpServletResponse response){
         if (memberService.emailDuplicateCheck(email)) {
